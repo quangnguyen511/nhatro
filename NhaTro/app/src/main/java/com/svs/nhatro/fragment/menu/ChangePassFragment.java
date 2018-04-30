e6aeeed04c21f0c37b17813d515bcd22b72d5ec8
Quang Nguyen <quang.nguyen@apps-cyclone.com>
2018-04-30 21:18:22 +0700

    + change pass page

---
+++ NhaTro/app/src/main/java/com/svs/nhatro/fragment/menu/ChangePassFragment.java
@@ -0,0 +1,48 @@
+package com.svs.nhatro.fragment.menu;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.svs.nhatro.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ChangePassFragment extends Fragment {
+
+
+    @BindView(R.id.fragChangePass_etNewPass)
+    EditText etNewPass;
+    @BindView(R.id.fragChangePass_etConfirmPass)
+    EditText etConfirmPass;
+    Unbinder unbinder;
+
+    public ChangePassFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_change_pass, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+}
