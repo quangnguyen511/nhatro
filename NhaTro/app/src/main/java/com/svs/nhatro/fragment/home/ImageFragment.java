3eab8a1e21fa8408aed8902451f8105e6fefc420
Quang Nguyen <quang.nguyen@apps-cyclone.com>
2018-04-30 20:42:49 +0700

    + image detail page

---
+++ NhaTro/app/src/main/java/com/svs/nhatro/fragment/home/ImageFragment.java
@@ -0,0 +1,41 @@
+package com.svs.nhatro.fragment.home;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.svs.nhatro.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ImageFragment extends Fragment {
+
+    @BindView(R.id.fragImage_imvImage)
+    ImageView imvImage;
+
+    private Unbinder unbinder;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_image, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+}
